f941050691729c505d3ae8a9459751dc63c240bc
Alex <alex.evant1448@gmail.com>
2024-06-04 15:29:24 +0300

    Add Welcome_To_The_City

---
+++ Welcome_To_The_City/Greeting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome_To_The_City
+{
+    internal class Greeting
+    { 
+        public static string SayHello(string[] name, string city, string state)
+        {
+            string nameOfPerson = name[0];           // Getting a name 
+
+            for (int i = 1; i < name.Length; i++)
+            {
+                nameOfPerson += " " + name[i];
+            }
+            
+
+            string nameOfCity = city;       // Gettting a name of city
+            string nameOfState = state;     // Getting a name of state
+
+            string sayHello = "Hello, " + nameOfPerson +
+                              "! Welcome to " + nameOfCity +
+                              ", " + nameOfState + "!";
+
+            return sayHello;
+        }
+    }
+}
---
+++ Welcome_To_The_City/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Welcome_To_The_City
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name1 = { "John", "Smith" };
+            string nameOfCity1 = "Phoenix";
+            string nameOfState1 = "Arizona";
+
+            string hello = Greeting.SayHello(name1, nameOfCity1, nameOfState1);
+            Console.WriteLine(hello);
+        }
+    }
+}
