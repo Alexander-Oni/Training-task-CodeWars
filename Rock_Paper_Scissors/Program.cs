3c7af27f193460312de2f0b9bf5537209552802e
Alex <alex.evant1448@gmail.com>
2024-06-04 15:27:09 +0300

    Add Rock_Paper_Scissors

---
+++ Rock_Paper_Scissors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string p1 = Console.ReadLine();
+            string p2 = Console.ReadLine();
+        }
+    }
+    public class Kata
+    {
+        public string Rps(string p1, string p2)
+        {
+            if (p1 == p2)
+                return "Draw!";
+
+            if (((p1 == "rock") && (p2 == "scissors")) ||
+                ((p1 == "scissors") && (p2 == "paper")) ||
+                ((p1 == "paper") && (p2 == "rock")))
+            {
+                return "Player 1 won!";
+            }
+            else
+            {
+                return "Player 2 won!";
+            }
+        }
+    }
+}
