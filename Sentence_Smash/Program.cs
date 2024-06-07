ad02b6d8b88f84e19243978240f4e08e836a2827
Alexander-Oni <alex.evant1448@gmail.com>
2024-06-07 19:11:50 +0300

    Add SentenceSmash method

---
+++ Sentence_Smash/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence_Smash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] testArr = { "Hello", "world", "this", "is", "great" };
+
+            Console.WriteLine(SentenceSmash(testArr));
+        }
+
+        static string SentenceSmash(string[] array)
+        {
+            string finalString = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                finalString += " " + array[i];
+            }
+
+            return finalString;
+        }
+    }
+}
