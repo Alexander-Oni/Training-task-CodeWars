de65b795f5f38a1a217c4a8af0f12c7f9b737efe
Alexander-Oni <alex.evant1448@gmail.com>
2024-06-07 18:28:00 +0300

    Add EvenOrOdd program

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++) 
+                Console.WriteLine(EvenOrOdd(i));
+
+            Console.ReadKey();
+        }
+
+        static string EvenOrOdd(int number)
+        {
+            return (number % 2 == 0) ? "Even" : "Odd";
+        }
+    }
+}
