5b2c38f6905d1f801d56a08c63fb11a29fef3b72
Alexander-Oni <alex.evant1448@gmail.com>
2024-06-24 18:32:24 +0300

    Add InvertValues method

---
+++ Invert_values/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invert_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] InvertValues(int[] inputArray)
+        {
+            int[] outputArray = new int[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+               outputArray[i] = -inputArray[i];
+            }
+
+            return outputArray;
+        }
+    }
+}
