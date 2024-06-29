1d09fe8a1ddb03bfb959ecc62f6a33ef253df39d
Alexander-Oni <alex.evant1448@gmail.com>
2024-06-29 09:11:52 +0300

    Add AbrevName method

---
+++ Abriviate_name/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abriviate_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string AbrevName(string name)
+        {
+            string abriviate = name[0] + ".";
+            for (int i = 1; i < name.Length; i++)
+            {
+                if (name[i] == ' ')
+                    abriviate += name[i + 1];
+            }
+
+            return abriviate.ToUpper();
+        }
+
+    }
+}
