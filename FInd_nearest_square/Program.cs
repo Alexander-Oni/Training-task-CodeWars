f4a4e250502e83c967209ccab2519cb02ec48147
Alexander-Oni <alex.evant1448@gmail.com>
2024-06-29 10:30:42 +0300

    Add NearestSq method

---
+++ FInd_nearest_square/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FInd_nearest_square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int NearestSq(int number)
+        {
+            int minNearSquare = (int)Math.Pow((int)Math.Sqrt(number), 2);
+            int maxNearSquare = (int)Math.Pow((int)Math.Sqrt(number) + 1, 2);
+
+            int nearSquare = ((number - minNearSquare) < (maxNearSquare - number)) ? minNearSquare :  maxNearSquare;
+
+            return nearSquare;
+        }
+    }
+}
