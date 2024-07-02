11c1980fa3441b29d581ba03ea70f82570be7105
Alexander-Oni <alex.evant1448@gmail.com>
2024-07-02 20:56:17 +0300

    Add PositiveSum method

---
+++ Sum_of_positive/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_positive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int PositiveSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++) 
+                if (array[i] > 0) sum += array[i];
+            return sum;
+        }
+    }
+}
